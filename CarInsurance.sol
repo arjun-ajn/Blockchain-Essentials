a43004ae5ed5b398451f7a9ded47aeb29e8f21b3
arjun-ajn <47026689+arjun-ajn@users.noreply.github.com>
2020-08-30 11:58:22 +0530

    LetsUpgrade Blockchain Es Batch 2 - Online Exam
    Car Insurance & Employee Data code & transaction files.

---
+++ CarInsurance.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.4.18 <= 0.6.12;
+
+contract CarInsurance{
+    
+    string custName;
+    string custAddress;
+    string carModel;
+    string date;
+    string insurerName;
+    string insuranceID;
+	int insureAmount;
+    
+    function CarInsurance(string newcustName, string newcustAddress, string newCarModel, string newDate, 
+	string newInsurerName, string newinsuranceID, int newInsureAmount) public {
+        custName = newcustName;
+        custAddress = newcustAddress;
+        carModel = newCarModel;
+        date = newDate;
+        insurerName = newInsurerName;
+        insuranceID = newinsuranceID;
+        insureAmount = newInsureAmount;
+    }
+    
+    function getCarInsurance() public view returns (string, string, string, string, string, string, int){
+        return(custName, custAddress, carModel, date, insurerName, insuranceID, insureAmount);
+    }
+    
+}
---
+++ EmployeeData.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.4.18 <= 0.6.12;
+
+contract EmployeeData{
+    
+    string empName;
+    string empAddrs;
+    string empSocialId;
+    string empId;
+	int empSalary;
+    
+    function EmployeeData(string newEmpName, string newEmpAddrs, string newEmpSocialId, string newEmpId, int newEmpSalary) public {
+        
+        empName = newEmpName;
+        empAddrs = newEmpAddrs;
+		empSocialId = newEmpSocialId;
+        empId = newEmpId;
+        empSalary = newEmpSalary;
+        
+    }  
+    
+    function getEmployeeData() public view returns(string, string, string, string, int) {
+        return(empName, empAddrs, empSocialId, empId, empSalary);
+    }
+    
+}
